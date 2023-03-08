323fa4643ff0fa6ef57256d581ab9e358859ec14
jr-philip <philipmwaura48@gmail.com>
2023-03-07 18:30:14 -0600

    app.ts

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,6 +1,24 @@
 import React from 'react';
 import './App.css';
 
+// types of typescript
+/* 
+1. let name: string;
+2. let age; number;
+3.let isStudent : boolean;
+4.let hobbies: string[];
+5.let role: [number, string] "tuple is a type of ts which contains a fixed amount of value  and types that are defined during declaration" */
+
+type People ={
+  name: string;
+  age: number;// if you want age to be optional use "age?: number" 
+}
+
+let person : People= {
+  name: "phil",  
+  age: 25,
+}
+
 function App() {
   return <div className="App">Hello world</div>;
 }
