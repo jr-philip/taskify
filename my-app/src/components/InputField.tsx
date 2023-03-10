738f33679a3961fe5f802f9f458abab53c7d1b8e
jr-philip <philipmwaura48@gmail.com>
2023-03-09 20:11:52 -0600

    input  field and Go button

--- my-app/src/components/InputField.tsx
+++ my-app/src/components/InputField.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 
 const InputField = () => {
-  return <div>Input</div>;
+  return <form className='input'>
+            <input type="input" placeholder='Enter a task' className='input_box' /> 
+            <button className='input_submit' type='submit'>Go</button>
+        </form>
 };
 
 export default InputField
