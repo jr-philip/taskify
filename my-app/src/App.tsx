78ee16b7c39a641ed881ce6c02c388e2b8b45823
jr-philip <philipmwaura48@gmail.com>
2023-03-12 08:27:05 -0500

    useState

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { updateExpressionStatement } from 'typescript';
 import './App.css';
 import InputField from './components/InputField';
 
 
 
 const App: React.FC = () => {
+
+  const [todo, setTodo] = useState<string>("")
+
   return (
     <div className="App">
       <span className="heading">TASKIFY</span>
