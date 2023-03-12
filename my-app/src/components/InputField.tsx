978a6c3ab806f3e7a44a2dcb53ad7fd0e4099979
jr-philip <philipmwaura48@gmail.com>
2023-03-12 08:48:19 -0500

    props

--- my-app/src/components/InputField.tsx
+++ my-app/src/components/InputField.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import './style.css';
 
-const InputField = () => {
+interface Props{
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;//coz setTodo is function,i'll hover on in App.tsx and then copy &paste the value that it will give me
+}
+
+const InputField = ({ todo, setTodo }: Props) => {
   return <form className='input'>
             <input type="input" placeholder='Enter a task' className='input_box' /> 
             <button className='input_submit' type='submit'>Go</button>
