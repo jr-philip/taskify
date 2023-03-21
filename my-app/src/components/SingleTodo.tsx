5f18ecf84b08f3698b3ad94030298a2a32d00f3b
jr-philip <philipmwaura48@gmail.com>
2023-03-20 19:11:43 -0500

    3 SPAN

--- my-app/src/components/SingleTodo.tsx
+++ my-app/src/components/SingleTodo.tsx
@@ -1,6 +1,7 @@
-
 import React from 'react'
 import { Todo } from '../model'
+import { AiFillEdit, AiFillDelete } from "react-icons/ai";
+import { MdDone } from "react-icons/md";
 
 
 type Props = {
@@ -10,11 +11,24 @@ type Props = {
 }
 
 const SingleTodo = ({ todo, todos, setTodos } :Props) => {
-  return (
+  return <form className='todos_single'>
+    <span className='todos_single--text'>
+      {todo.todo}
+    </span>
     <div>
-      
+      <span className='icon'>
+        <AiFillEdit />
+      </span>
+      <span className='icon'>
+        <AiFillDelete />
+      </span>
+      <span className='icon'>
+        <MdDone/>
+      </span>
     </div>
-  )
+
+  </form>
+  
 }
 
 export default SingleTodo
