72bd9383b3d50b2bb3d032f86f1d04863f7e9c6a
jr-philip <philipmwaura48@gmail.com>
2023-03-27 17:40:19 -0500

    Update TodoList.tsx

--- my-app/src/components/TodoList.tsx
+++ my-app/src/components/TodoList.tsx
@@ -10,15 +10,24 @@ interface Props{
 
 const TodoList :React.FC<Props> = ({ todos, setTodos }: Props) => {
   return (
-    <div className='todos'>
-        {todos.map((todo) => (
+    <div className='container'>
+      <div className='todos'>
+        <span className='todos_heading'>
+          Active Tasks
+        </span>
+        {
+          todos.map((todo) => (
             <SingleTodo 
               todo={todo} 
-              key={todo.id}
               todos={todos}
-              setTodos={setTodos}
-            />
-        ))}
+              key={todo.id} 
+              setTodos={setTodos}/>
+          ))
+        }
+      </div>
+      <div className='todos remove'>
+
+      </div>
     </div>
   );
 };
