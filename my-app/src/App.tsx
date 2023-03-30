b74d960ebb03d5af743975550d70748bcb2c9637
jr-philip <philipmwaura48@gmail.com>
2023-03-30 18:19:34 -0500

    update

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,98 +1,10 @@
-import React, { useState } from "react";
-import "./App.css";
-import InputField from "./components/InputField";
-import TodoList from "./components/TodoList";
-import { DragDropContext, DropResult } from "react-beautiful-dnd";
-import { Todo } from "./model";
-import { Learn } from "./components/Learn";
-
-const App: React.FC = () => {
-  const [todo, setTodo] = useState<string>("");
-  const [todos, setTodos] = useState<Array<Todo>>([]);
-  const [CompletedTodos, setCompletedTodos] = useState<Array<Todo>>([]);
-
-  const handleAdd = (e: React.FormEvent) => {
-    e.preventDefault();
-
-    if (todo) {
-      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
-      setTodo("");
-    }
-  };
-
- 
-
-  const onTest = (result: any) => {
-    console.log('test')
-    if (!result.destination) {
-      return;
-    }
-  }
-  const onDragEnd = (result: DropResult) => {
-    const { destination, source } = result;
-
-    console.log(result);
-
-    if (!destination) {
-      return;
-    }
-
-    if (
-      destination.droppableId === source.droppableId &&
-      destination.index === source.index
-    ) {
-      return;
-    }
-
-    let add;
-    let active = todos;
-    let complete = CompletedTodos;
-    // Source Logic
-    if (source.droppableId === "TodosList") {
-      add = active[source.index];
-      active.splice(source.index, 1);
-    } else {
-      add = complete[source.index];
-      complete.splice(source.index, 1);
-    }
-
-    // Destination Logic
-    if (destination.droppableId === "TodosList") {
-      active.splice(destination.index, 0, add);
-    } else {
-      complete.splice(destination.index, 0, add);
-    }
-
-    setCompletedTodos(complete);
-    setTodos(active);
-  };
-
-  return (
-    <Learn />
-    // <DragDropContext onDragEnd={onDragEnd} >
-    //   <div className="App">
-    //     <span className="heading">Taskify</span>
-    //     <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
-    //     <TodoList
-    //       todos={todos}
-    //       setTodos={setTodos}
-    //       CompletedTodos={CompletedTodos}
-    //       setCompletedTodos={setCompletedTodos}
-    //     />
-    //   </div>
-    //  </DragDropContext>
-  );
-};
-
-export default App;
-
-/*import React, { useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
 import { Todo } from './model';
 import TodoList from './components/TodoList';
 import { DragDropContext, DropResult } from 'react-beautiful-dnd';
-;
+
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("")//creating the state of todo and setTodo
@@ -153,7 +65,7 @@ const App: React.FC = () => {
         <TodoList 
           todos={todos} 
           setTodos={setTodos}
-          completedTodos={completedTodos}
+          CompletedTodos={completedTodos}
           setCompletedTodos={setCompletedTodos}
         />
       </div>
@@ -163,7 +75,7 @@ const App: React.FC = () => {
 
 export default App;
 
-
+/*
 // types of typescript
 
 1. let name: string;
@@ -210,4 +122,5 @@ function printName(name:string){// defining a function
 }
 printName("phil")
 
-let personName: unknown;// if i dont have a clue what value is going to be returned , use "UNKNOWN"*/
+let personName: unknown;// if i dont have a clue what value is going to be returned , use "UNKNOWN"
+*/
