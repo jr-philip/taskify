bd7449c86ce2b03fbf74b998e1c67d038bbe25c0
jr-philip <philipmwaura48@gmail.com>
2023-03-29 19:44:11 -0500

    practice

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -4,6 +4,7 @@ import InputField from "./components/InputField";
 import TodoList from "./components/TodoList";
 import { DragDropContext, DropResult } from "react-beautiful-dnd";
 import { Todo } from "./model";
+import { Learn } from "./components/Learn";
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
@@ -19,6 +20,14 @@ const App: React.FC = () => {
     }
   };
 
+ 
+
+  const onTest = (result: any) => {
+    console.log('test')
+    if (!result.destination) {
+      return;
+    }
+  }
   const onDragEnd = (result: DropResult) => {
     const { destination, source } = result;
 
@@ -59,14 +68,92 @@ const App: React.FC = () => {
   };
 
   return (
+    <Learn />
+    // <DragDropContext onDragEnd={onDragEnd} >
+    //   <div className="App">
+    //     <span className="heading">Taskify</span>
+    //     <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+    //     <TodoList
+    //       todos={todos}
+    //       setTodos={setTodos}
+    //       CompletedTodos={CompletedTodos}
+    //       setCompletedTodos={setCompletedTodos}
+    //     />
+    //   </div>
+    //  </DragDropContext>
+  );
+};
+
+export default App;
+
+/*import React, { useState } from 'react';
+import './App.css';
+import InputField from './components/InputField';
+import { Todo } from './model';
+import TodoList from './components/TodoList';
+import { DragDropContext, DropResult } from 'react-beautiful-dnd';
+;
+
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("")//creating the state of todo and setTodo
+  const [todos, setTodos] = useState<Todo[]>([])//these is how you create an array of a type or interface
+  const [completedTodos,setCompletedTodos ] = useState<Todo[]>([])
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if(todo){// check and see what is the todo
+      setTodos([...todos, {id:Date.now(), todo, isDone:false}])// date.now() will gennerate random id
+      setTodo("")//setTodo will be set to empty string so as to empty the input field
+    }
+  };
+
+ const onDragEnd = (result:DropResult) => {
+   const { destination, source } = result;
+    console.log(result)
+
+    if(!destination) {
+      return
+    }
+
+    if(
+      destination.droppableId === source.droppableId && 
+      destination.index === source.index
+      ){
+       return
+      }
+
+    let add,
+        active = todos,
+        complete = completedTodos;
+
+    if(source.droppableId === 'TodoList'){
+      add = active[source.index];
+      active.splice(source.index, 1)
+    }else {
+      add = complete[source.index];
+      complete.splice(source.index, 1)
+    }
+
+   if (destination.droppableId === 'TodoList') {
+     active.splice(destination.index, 0, {...add, isDone:false})
+   } else {
+     complete.splice(source.index, 0, { ...add, isDone: true })
+   }
+
+   setCompletedTodos(complete)
+   setTodos(active)
+ }
+
+  return (
     <DragDropContext onDragEnd={onDragEnd}>
       <div className="App">
-        <span className="heading">Taskify</span>
+        <span className="heading">TASKIFY</span>
         <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
-        <TodoList
-          todos={todos}
+        <TodoList 
+          todos={todos} 
           setTodos={setTodos}
-          CompletedTodos={CompletedTodos}
+          completedTodos={completedTodos}
           setCompletedTodos={setCompletedTodos}
         />
       </div>
@@ -75,8 +162,10 @@ const App: React.FC = () => {
 };
 
 export default App;
+
+
 // types of typescript
-/* 
+
 1. let name: string;
 2. let age; number;
 3.let isStudent : boolean;
@@ -93,19 +182,6 @@ let person : People= {
   age: 25,
 }
 
-const App: React.FC = () => {
-  const [todo, setTodo] = useState<string>("")//creating the state of todo and setTodo
-  const [todos, setTodos] = useState<Todo[]>([])//these is how you create an array of a type or interface
-  const [completedTodos,setCompletedTodos ] = useState<Todo[]>([])
-
-  const handleAdd = (e: React.FormEvent) => {
-    e.preventDefault();
-
-    if(todo){// check and see what is the todo
-      setTodos([...todos, {id:Date.now(), todo, isDone:false}])// date.now() will gennerate random id
-      setTodo("")//setTodo will be set to empty string so as to empty the input field
-    }
-  };
 let age : number | string; // union method combines both types 
 age = "two";
 
---
+++ my-app/src/components/LastName.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react'
+
+interface IProps {
+    jina: string
+}
+export const LastName: FC<IProps> = ({jina}) => {
+  return (
+    <div>
+      <h1>Hey Phil {jina}</h1>
+    </div>
+  );
+}
+
+
